d1acf1d00c92adfac2d7b45f7727c3a3f6b86316
Shyaman Dhanushka <ksdhanushka777@gmail.com>
2024-06-06 18:12:48 +0530

    Feat: Created recipesSlice

--- src/store/rootReducer.js
+++ src/store/rootReducer.js
@@ -1,8 +1,9 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { authenticationApi } from "../redux/auth/api.js";
 import { recipesApi } from "../redux/recieps/api.js";
-
+import recipes from "../redux/recipeSlice.js";
 export const rootReducer = combineReducers({
+  recipes,
   [authenticationApi.reducerPath]: authenticationApi.reducer,
   [recipesApi.reducerPath]: recipesApi.reducer,
 });
